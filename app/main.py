24c972961f42c776a5c08639e067d82299d41a03
MishaRocks <tehobox@yandex.ru>
2023-07-05 01:28:48 +0300

    api and database done

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from app.src.dbmanager import DBManager
 from app.src.hh_api import HeadHunterAPI
+from app.src.utils import create_tables, add_to_table
+from app.src.constants import employers_list
+
 
 dbm = DBManager()
 hh = HeadHunterAPI()
 
 if __name__ == '__main__':
-    print(hh.get_company_by_id(1740))
+    create_tables()
+    add_to_table(employers_list)
--- app/src/hh_api.py
+++ app/src/hh_api.py
@@ -3,11 +3,11 @@ import requests
 
 class HeadHunterAPI:
 
-    def _get_company_vacancies(self, employer_id: int) -> list:
+    def get_company_vacancies(self, employer_id: int) -> list:
         """
-                    Отправляем запрос на получение списка вакансий по работодателю к Апи HeadHunter
-                    :return: список вакансий
-                """
+        Отправляем запрос на получение списка вакансий по работодателю к Апи HeadHunter
+        :return: список вакансий
+        """
         params = {
             'area': 1,  # Поиск осуществляется по вакансиям города Москва(1)
             'page': 0,  # Индекс страницы поиска на HH
@@ -15,13 +15,38 @@ class HeadHunterAPI:
         }
         req = requests.get(f'https://api.hh.ru/vacancies?employer_id={employer_id}', params)  # Посылаем запрос к API
         data = req.json()
-        return data
 
-    def _get_company_by_id(self, employer_id: int) -> dict:
+        hh_list = []
+        for v in data["items"]:
+            hh_dict = {
+                'id': int(v['id']),
+                'title': v["name"],
+                'payment': v["salary"]["from"] if v["salary"] else None,
+                'date': v["published_at"],
+                'description': v["snippet"]["responsibility"],
+                'candidate': v["snippet"]["requirement"],
+                'url': v["alternate_url"],
+                'employer_id': employer_id
+            }
+            if hh_dict['payment'] is not None:
+                hh_list.append(hh_dict)
+
+        return hh_list
+
+    def get_company_by_id(self, employer_id: int) -> dict:
         """
-                            Отправляем запрос на получение списка вакансий по работодателю к Апи HeadHunter
-                            :return: список вакансий
-                        """
+        Отправляем запрос на получение списка вакансий по работодателю к Апи HeadHunter
+        :return: словарь с данными о компании
+        """
         req = requests.get(f'https://api.hh.ru/employers/{employer_id}')
         data = req.json()
-        return data
+        hh_company = {
+            "employer_id": int(employer_id),
+            "title": data['name'],
+            "industries": data['industries'][0]['name'] if data['industries'] else None,
+            "area": data['area']['name'],
+            "open_vacancies": data['open_vacancies'],
+            "url": data['alternate_url']
+        }
+
+        return hh_company
